ad7230f095f4d2c5f06793d31bc0e246800ebb64
unknown <andreinichol.e.castro.dev@gmail.com>
2022-01-23 14:24:01 +0800

    learned about bodyParser and  redirecting routes

--- app.js
+++ app.js
@@ -1,17 +1,28 @@
-const http = require("http");
-const express = require('express')
 
+const express = require('express')
+const bodyParser = require('body-parser')
 
 const app = express();
 
-app.use("/add-product",(req, res, next) => {
-    res.send('<h1>Add products</h1>'); // allows us to send a response
+//parser
+// register a middleware, will all the next in the end
+//do the request parsing 
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use("/add-product", (req, res, next) => {
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+    // dont call next if you are sending a response
 });
+
+app.use('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+})
 
 
 
 // adds middleware function,s
-app.use("/",(req, res, next) => {
+app.use("/", (req, res, next) => {
     res.send('<h1> hello </h1>'); // allows us to send a response
 });
 
