6dc30fb068b02b5a2ad1d8fa718dade553bd48ba
unknown <andreinichol.e.castro.dev@gmail.com>
2022-01-23 13:47:09 +0800

    learned how to use express, create the app function, the use function  and the express listen function

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const http = require("http");
+const express = require('express')
 
 
+const app = express();
+
+// adds middleware function,
+app.use((req, res, next) => {
+    next(); // allows the tunnel of another middle ware
+    // basically calls another middle ware
+});
+app.use((req, res, next) => {
+    res.send('<h1> hello </h1>'); // allows us to send a response
+});
 
 
-const server = http.createServer()
-
-server.listen(3000);
+app.listen(3000)
