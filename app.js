12200bceec8556e63bdbfba59e465737998a1396
unknown <andreinichol.e.castro.dev@gmail.com>
2022-01-23 13:58:52 +0800

    learned about routing on the use function

--- app.js
+++ app.js
@@ -4,12 +4,14 @@ const express = require('express')
 
 const app = express();
 
-// adds middleware function,
-app.use((req, res, next) => {
-    next(); // allows the tunnel of another middle ware
-    // basically calls another middle ware
+app.use("/add-product",(req, res, next) => {
+    res.send('<h1>Add products</h1>'); // allows us to send a response
 });
-app.use((req, res, next) => {
+
+
+
+// adds middleware function,s
+app.use("/",(req, res, next) => {
     res.send('<h1> hello </h1>'); // allows us to send a response
 });
 
